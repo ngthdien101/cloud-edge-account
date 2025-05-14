9206f1d2b834dfeff7c9d29c76ddfe359b9d38dc
Dien Nguyen <62537779+ngthdien101@users.noreply.github.com>
2025-05-15 11:33:44 +1200

    Create outputs.tf

---
+++ modules/cloudfront/outputs.tf
@@ -0,0 +1,8 @@
+/*
+  Outputs from CloudFront module
+*/
+
+output "cloudfront_domain_name" {
+  description = "Domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.this.domain_name
+}
